febd5004814817a7e861b043b20531d167c6f91b
monsieur486 <monsieur486@gmail.com>
2026-05-31 12:48:18 +0200

    feat(ms-client): page /account (mon compte + lien reset password Keycloak)

--- src/main/resources/templates/ms-platform/ms-client/src/main/java/com/mr486/msplatform/client/config/ClientProperties.java
+++ src/main/resources/templates/ms-platform/ms-client/src/main/java/com/mr486/msplatform/client/config/ClientProperties.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /** Catalogue des resources exposées par l'UI CRUD (injecté par le générateur dans application.yml). */
 @ConfigurationProperties(prefix = "client")
-public record ClientProperties(List<ResourceEntry> resources) {
+public record ClientProperties(List<ResourceEntry> resources, String keycloakAccountUrl) {
 
     public record ResourceEntry(String serviceName, String routePrefix, String label, String role) {}
 }
