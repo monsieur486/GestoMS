3d4c7342462df32cbbdb6d98601a7132117d4d12
monsieur486 <monsieur486@gmail.com>
2026-05-31 12:46:44 +0200

    feat(ms-client): page publique /public (landing marketing)

---
+++ src/main/resources/templates/ms-platform/ms-client/src/main/java/com/mr486/msplatform/client/web/PublicController.java
@@ -0,0 +1,13 @@
+package com.mr486.msplatform.client.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublicController {
+
+    @GetMapping("/public")
+    public String publicPage() {
+        return "public";
+    }
+}
